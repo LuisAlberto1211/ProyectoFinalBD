f06215ede7435d79142cbf6d601bd7bdacad342c
Luis Alberto Ortega <mew_9010@hotmail.com>
2015-12-09 10:56:57 -0600

    Segundo trigger agregado

--- triggers.sql
+++ triggers.sql
@@ -1,6 +1,6 @@
 --AGREGAR ESTATUS AL HISTORIAL CUANDO SE MODIFIQUE SU VALOR
 CREATE OR REPLACE TRIGGER historico_status_trigger
-AFTER UPDATE OF STATUS_ARTICULO_ID ON ARTICULO
+AFTER UPDATE OR INSERT OF STATUS_ARTICULO_ID ON ARTICULO
 FOR EACH ROW
 DECLARE
   v_status_id     NUMBER(10,0);
@@ -28,4 +28,26 @@ BEGIN
       v_status_id,
       v_fecha_status
     );
+END;
+
+--Cambiar el STATUS de un articulo una vez que pasa a ser publicado
+CREATE OR REPLACE TRIGGER articulo_publicado_trigger
+BEFORE INSERT ON ARTICULO_PUBLICACION
+FOR EACH ROW
+DECLARE
+  v_articulo_publicacion_id NUMBER(10,0);
+  v_status_id               NUMBER(10,0);
+  v_articulo_id             NUMBER(10,0);
+  v_fecha_status            DATE;
+BEGIN
+  SELECT articulo_publicacion_seq.NEXTVAL INTO v_articulo_publicacion_id FROM dual;
+  SELECT STATUS_ARTICULO_ID INTO v_status_id FROM STATUS_ARTICULO WHERE DESCRIPCION = 'PUBLICADO';
+
+  v_fecha_status := sysdate;
+  v_articulo_id := :NEW.ARTICULO_ID;
+
+  DBMS_OUTPUT.PUT_LINE('ARTICULO: ' || :NEW.ARTICULO_ID || ' PUBLICADO');
+
+  UPDATE ARTICULO SET FECHA_STATUS = v_fecha_status, STATUS_ARTICULO_ID = v_status_id WHERE ARTICULO_ID = v_articulo_id;
+
 END;
